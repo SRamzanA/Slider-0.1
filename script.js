b1147bd8d18bb940e6eed6a62968342550612bda
SRamzanA <ramzansmajlov14@gmail.com>
2024-03-31 15:28:42 +0700

    ю

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+let images = document.getElementById("images")
 
+let slide = [
+    "img/image1.webp",
+    "img/image2.webp",
+    "img/image3.webp",
+    "img/image4.webp",
+    "img/image5.webp"
+]
 
+images.src = slide[0]
 
-
-
-
-
+let numSlide = 0
+function imgBefore() {
+    if (numSlide == 0) {
+        numSlide = slide.length-1
+        images.src = slide[numSlide]
+    } else {
+        numSlide -= 1
+        images.src = slide[numSlide]
+    }
+}
+function imgAfter() {
+    if (numSlide == slide.length-1) {
+        numSlide = 0
+        images.src = slide[numSlide]
+    } else {
+        numSlide += 1
+        images.src = slide[numSlide]
+    }
+}
 
 
 
